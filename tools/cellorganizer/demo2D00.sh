d76bbddcc1b6d7d0186acd0902bc5ac11e63a1b4
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-04-06 17:11:20 -0400

    Updated shell script.

--- tools/cellorganizer/demo2D00.sh
+++ tools/cellorganizer/demo2D00.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 export PATH=$PATH:$(dirname $0)
+WORKING_DIRECTORY=`pwd`
+CELLORGANIZER=/Users/icaoberg/Documents/MATLAB/cellorganizer
 
 SEED=$1
 NUMBER_OF_IMAGES=$2
 
-if [ ! -f cellorganizer_v2.4.zip ]; then
-	curl -o "cellorganizer_v2.4.zip" "http://cellorganizer.org/Downloads/v2.0/cellorganizer_v2.4.zip"
-	unzip -o cellorganizer_v2.4.zip
-	rm -fv cellorganizer_v2.4.zip
-fi
-
-cd cellorganizer_v2.4
-/Applications/MATLAB_R2015a.app/bin/matlab -nodesktop -nosplash -r "setup(true);param.seed="$SEED";param.numberOfSynthesizedImages="$NUMBER_OF_IMAGES";demo2D00(param); pwd, copyfile( './images/demo2D00_1.tif', '../../../../' ); exit;"
+ln -s $CELLORGANIZER $(pwd)/cellorganizer
+cd cellorganizer
+/Applications/MATLAB_R2015a.app/bin/matlab -nodesktop -nosplash -r "setup(true);param.seed="$SEED";param.numberOfSynthesizedImages="$NUMBER_OF_IMAGES";demo2D00(param); pwd, copyfile( './images/demo2D00_1.tif', '"$WORKING_DIRECTORY"' ); exit;"
+cd $WORKING_DIRECTORY
+rm -fv cellorganizer
