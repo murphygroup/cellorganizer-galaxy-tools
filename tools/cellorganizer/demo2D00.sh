c04a24b981ed5b67eed9ce87189746ca9a85c46f
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-04-25 17:48:26 +0200

    Fixed merge conflict.

--- tools/cellorganizer/demo2D00.sh
+++ tools/cellorganizer/demo2D00.sh
@@ -3,12 +3,63 @@
 export PATH=$PATH:$(dirname $0)
 WORKING_DIRECTORY=`pwd`
 CELLORGANIZER=/Users/icaoberg/Documents/MATLAB/cellorganizer
+MATLAB=/Applications/MATLAB_R2015a.app/bin/matlab
 
-SEED=$1
-NUMBER_OF_IMAGES=$2
+if [ "$#" -ne 1 ]; then
+  SEED=12345
+  NUMBER_OF_IMAGES=1
+  COMPRESSION='lzw'
+else
+  SEED=$1
+  NUMBER_OF_IMAGES=$2
+  COMPRESSION=$3
+fi
+
+echo "
+% demo2D00
+%
+% Synthesize one 2D image from all vesicles models found in the models
+% folder included in this distribution.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DO NOT MODIFY THIS BLOCK
+addpath( genpath([pwd filesep 'cellorganizer']));
+
+options.targetDirectory = pwd;
+options.prefix = 'output';
+options.debug = false;
+options.verbose = true;
+options.display = false;
+
+options.seed=$SEED;
+try
+    state = rng( options.seed );
+catch err
+    state = rand( 'seed', options.seed ); %#ok<RAND>
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+options.numberOfSynthesizedImages=$NUMBER_OF_IMAGES
+options.compression = '$COMPRESSION';
+options.targetDirectory = pwd;
+options.debug = false;
+options.verbose = true;
+options.display = false;
+
+tic
+answer = slml2img( {'./cellorganizer/models/2D/nucleolus.mat', ...
+  './cellorganizer/models/2D/endosome.mat', ...
+  './cellorganizer/models/2D/mitochondrion.mat', ...
+  './cellorganizer/models/2D/lysosome.mat'}, options );
+toc
+
+exit;" > script.m
 
 ln -s $CELLORGANIZER $(pwd)/cellorganizer
-cd cellorganizer
-/Applications/MATLAB_R2015a.app/bin/matlab -nodesktop -nosplash -r "setup(true);param.seed="$SEED";param.numberOfSynthesizedImages="$NUMBER_OF_IMAGES";demo2D00(param); pwd, copyfile( './images/demo2D00_1.tif', '"$WORKING_DIRECTORY"' ); exit;"
+##$MATLAB -nodesktop -nosplash -r "script;"
+
 cd $WORKING_DIRECTORY
-rm -fv cellorganizer
+rm -fv ./cellorganizer
+
+echo "I pity the fool!" > output
+
+ls
