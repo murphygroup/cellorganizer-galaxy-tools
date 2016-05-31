15b001e1c9addfaabd07c679d67ec569e12764a0
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-05-31 01:06:26 -0400

    Include empty shell scripts.

---
+++ tools/cellorganizer/demo3D00.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export PATH=$PATH:$(dirname $0)
+CELLORGANIZER=/pylon1/mc4s8dp/icaoberg/galaxy/cellorganizer
+
+WORKING_DIRECTORY=`pwd`
+
+MATLAB=/opt/packages/matlab/R2016a/bin/matlab
+
+echo "
+exit;" > script.m
+
+echo "Running the following script in Matlab"
+cat script.m
+
+echo $WORKING_DIRECTORY
+ln -s $CELLORGANIZER $(pwd)/cellorganizer
+$MATLAB -nodesktop -nosplash -r "script;"
