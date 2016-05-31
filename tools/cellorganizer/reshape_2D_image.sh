5b6307ab116b81406803082af277e7f698fb35bc
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-05-30 23:18:13 -0400

    Included script to reshape 2D images.

---
+++ tools/cellorganizer/reshape_2D_image.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+export PATH=$PATH:$(dirname $0)
+CELLORGANIZER=/pylon1/mc4s8dp/icaoberg/galaxy/cellorganizer
+
+WORKING_DIRECTORY=`pwd`
+
+MATLAB=/opt/packages/matlab/R2016a/bin/matlab
+
+INPUT=$1
+NUMBER_OF_IMAGES=1
+COMPRESSION=$2
+
+echo "
+% reshape_2D_image
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DO NOT MODIFY THIS BLOCK
+addpath( genpath([pwd filesep 'cellorganizer']));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+file = $INPUT;
+img = tif2img( file );
+img2 = reshape( img, size(img, 1 ), [] );
+img2 = uint8(img2);
+imwrite( img2, ['output.png'] );
+
+exit;" > script.m
+
+echo "Running the following script in Matlab"
+cat script.m
+
+echo $WORKING_DIRECTORY
+ln -s $CELLORGANIZER $(pwd)/cellorganizer
+$MATLAB -nodesktop -nosplash -r "script;"
