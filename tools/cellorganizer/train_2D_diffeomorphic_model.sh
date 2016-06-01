e27cbcc6649f3a1776b48312608a4c3fb652a2ea
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-06-01 12:15:26 -0400

    Updated tool.

--- tools/cellorganizer/train_2D_diffeomorphic_model.sh
+++ tools/cellorganizer/train_2D_diffeomorphic_model.sh
@@ -7,24 +7,28 @@ WORKING_DIRECTORY=`pwd`
 
 MATLAB=/opt/packages/matlab/R2016a/bin/matlab
 
-NUMBER_OF_IMAGES=$1
+DATASET=$1
+NUMBER_OF_IMAGES=$2
+DOWNSAMPLE_FACTOR=$3
 
 echo "
 % mmbios04
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % DO NOT MODIFY THIS BLOCK
-addpath( genpath([pwd filesep 'cellorganizer']));
+cd ./cellorganizer
+setup(true)
+cd('$WORKING_DIRECTORY')
 
 options.verbose = true;
 options.debug = false;
 options.display = true;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% training component: main call to img2slml
+%% Training component: main call to img2slml
 
 % collection, modify these according to your needs
-directory = './cellorganizer/images/HeLa/2D/LAM/';
+directory = './cellorganizer/images/HeLa/2D/$DATASET/';
 dna = [ directory filesep 'orgdna' filesep 'cell[0-$NUMBER_OF_IMAGES].tif' ];
 cellm = [ directory filesep 'orgcell' filesep 'cell[0-$NUMBER_OF_IMAGES].tif' ];
 protein = [ directory filesep 'orgprot' filesep 'cell[0-$NUMBER_OF_IMAGES].tif' ];
@@ -32,18 +36,18 @@ options.masks = [ directory filesep 'crop' filesep 'cell[0-$NUMBER_OF_IMAGES].ti
 
 options.model.resolution = [ 0.49, 0.49 ];
 options.model.filename = 'model.mat';
-options.model.id = 'lamp2';
-options.model.name = 'lamp2';
+options.model.id = num2str(now);
+options.model.name = 'cellorganizer-on-galaxy';
 
 %set nuclei and cell model name
-options.nucleus.name = 'LAMP2';
-options.cell.model = 'LAMP2';
+options.nucleus.name = 'num2str(now)';
+options.cell.model = 'num2str(now)';
 
 %set the dimensionality of the model
 dimensionality = '2D';
 
 %documentation
-options.documentation.description = 'This model has been trained using demo2D04 from CellOrganizer';
+options.documentation.description = 'This model has been trained on CellOrganizer on Galaxy+Bridges';
 
 %set model type
 options.nucleus.class = 'framework';
@@ -52,7 +56,7 @@ options.nucleus.type = 'diffeomorphic';
 options.cell.type = 'diffeomorphic';
 options.train.flag = 'framework';
 
-options.downsampling = [5,5];
+options.downsampling = [$DOWNSAMPLE_FACTOR,$DOWNSAMPLE_FACTOR];
 
 %train the model
 success = img2slml( dimensionality, dna, cellm,[], options );
