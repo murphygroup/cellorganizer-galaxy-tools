4772ca8b54368052fccfe6b3269edf13e5372d9c
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-06-02 15:07:43 -0400

    Updated tool.

---
+++ tools/cellorganizer/synthesize_2D_diffeormorphic_instance.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+export PATH=$PATH:$(dirname $0)
+CELLORGANIZER=/pylon1/mc4s8dp/icaoberg/galaxy/cellorganizer
+
+WORKING_DIRECTORY=`pwd`
+
+MATLAB=/opt/packages/matlab/R2016a/bin/matlab
+
+INPUT=$1
+cp -v $INPUT $(pwd)/model.mat
+
+echo "
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DO NOT MODIFY THIS BLOCK
+cd ./cellorganizer
+setup(true);
+cd('$WORKING_DIRECTORY')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% specify model name and load model
+model_name = './model.mat';
+load(model_name);
+
+% show shape space by calling the function
+f = figure('visible','off');
+showShapeSpaceFigure(model);
+saveas( f, 'show_shape_space.png', 'png' );
+
+exit;" > script.m
+
+echo "Running the following script in Matlab"
+cat script.m
+
+echo $WORKING_DIRECTORY
+ln -s $CELLORGANIZER $(pwd)/cellorganizer
+$MATLAB -nodesktop -nosplash -r "script;"
