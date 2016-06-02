c362312caa539c206c2a9895d13630dc12440467
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-06-02 15:45:07 -0400

    Updated script to retrieve image using bash instead of python.

--- tools/cellorganizer/sample_from_public_database.sh
+++ tools/cellorganizer/sample_from_public_database.sh
@@ -4,6 +4,6 @@ export PATH=$PATH:$(dirname $0)
 
 WORKING_DIRECTORY=`pwd`
 
-python sample_from_public_database.py
-ls
-pwd
+OMERO_ID=`python -c "from __future__ import print_function; import random; print(random.choice(range(51, 251) + range(304, 425) + range(451, 532)), end='')"`
+
+wget -O output.tif -nc "http://omero.compbio.cs.cmu.edu:8080/webgateway/render_ome_tiff/i/"$OMERO_ID"/"
