cad13e9c38a30e1d8c72cc76f79bc44dc80ce64a
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-06-02 10:19:17 -0400

    Included tool for 3D image reshape.

---
+++ tools/cellorganizer/show_3D_image_reshape.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+export PATH=$PATH:$(dirname $0)
+CELLORGANIZER=/pylon1/mc4s8dp/icaoberg/galaxy/cellorganizer
+
+WORKING_DIRECTORY=`pwd`
+
+MATLAB=/opt/packages/matlab/R2016a/bin/matlab
+
+INPUT=$1
+NUMBER_OF_IMAGES=1
+COMPRESSION=$2
+
+ln -s $INPUT $(pwd)/output.zip
+unzip ./output.zip
+rm -fv output.zip
+
+ls
+
+echo "
+% reshape_2D_image
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DO NOT MODIFY THIS BLOCK
+addpath( genpath([pwd filesep 'cellorganizer']));
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+file = 'output.tif';
+disp( ['Loading image ' file])
+img = tif2img( file );
+img2 = reshape( img, size(img, 1 ), [] );
+img2 = uint8(img2);
+imwrite( img2, ['output.png'] );
+
+exit;" > script.m
+
+echo "Running the following script in Matlab"
+cat script.m
+
+echo $WORKING_DIRECTORY
+ln -s $CELLORGANIZER $(pwd)/cellorganizer
+echo $MATLAB -nodesktop -nosplash -r "script;"
