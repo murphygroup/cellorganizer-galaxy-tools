cf4826ac60eaedecc95e4eb13a5b2cc63a3e8bde
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-06-03 09:43:13 -0400

    Updated script.

---
+++ tools/cellorganizer/synthesize_3D_vesicular_instance.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+export PATH=$PATH:$(dirname $0)
+CELLORGANIZER=/pylon1/mc4s8dp/icaoberg/galaxy/cellorganizer
+
+WORKING_DIRECTORY=`pwd`
+
+MATLAB=/opt/packages/matlab/R2016a/bin/matlab
+
+INPUT=$1
+cp -v $INPUT $(pwd)/model.mat
+
+echo "
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DO NOT MODIFY THIS BLOCK
+cd ./cellorganizer
+setup(true);
+cd('$WORKING_DIRECTORY')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% specify model name and load model
+files = {'./model.mat'};
+options.synthesis = 'all';
+
+options.targetDirectory = pwd;
+options.prefix = 'example';
+options.compression = 'lzw';
+options.sampling.method = 'disc';
+
+slml2img( files, options );
+
+exit;" > script.m
+
+echo "Running the following script in Matlab"
+cat script.m
+
+echo $WORKING_DIRECTORY
+ln -s $CELLORGANIZER $(pwd)/cellorganizer
+$MATLAB -nodesktop -nosplash -r "script;"
+
+zip -rv output.zip example
+rm -rfv example
