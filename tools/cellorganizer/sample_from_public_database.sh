8b2b85c4281b49681f8f1651de81d4c25c8775a4
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2016-06-21 16:47:46 -0400

    Update script.

--- tools/cellorganizer/sample_from_public_database.sh
+++ tools/cellorganizer/sample_from_public_database.sh
@@ -6,4 +6,4 @@ WORKING_DIRECTORY=`pwd`
 
 OMERO_ID=`python -c "from __future__ import print_function; import random; print(random.choice(range(51, 251) + range(304, 425) + range(451, 532)), end='')"`
 
-wget -O output.tif -nc "http://omero.compbio.cs.cmu.edu:8080/webgateway/render_ome_tiff/i/"$OMERO_ID"/"
+wget --quiet -O output.tif -o /dev/null -nc "http://omero.compbio.cs.cmu.edu:8080/webgateway/render_ome_tiff/i/"$OMERO_ID"/"
