e9dcf77abf48dcff5ed23bc4c788f70466df41e3
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2016-06-27 01:51:30 -0400

    Cleaned shell script.

--- tools/cellorganizer/show_2D_image_reshape.sh
+++ tools/cellorganizer/show_2D_image_reshape.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 export PATH=$PATH:$(dirname $0)
-CELLORGANIZER=/pylon1/mc4s8dp/icaoberg/galaxy/cellorganizer
 
 WORKING_DIRECTORY=`pwd`
-
-MATLAB=/opt/packages/matlab/R2016a/bin/matlab
 
 INPUT=$1
 
@@ -28,9 +25,5 @@ imwrite( img2, ['output.png'] );
 
 exit;" > script.m
 
-echo "Running the following script in Matlab"
-cat script.m
-
-echo $WORKING_DIRECTORY
 ln -s $CELLORGANIZER $(pwd)/cellorganizer
 $MATLAB -nodesktop -nosplash -r "script;"
