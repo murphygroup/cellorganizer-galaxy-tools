23a1dc5d1e9c9c7cf96a3734de68b9f406795a5a
Tim Majarian <tmajarian@cmu.edu>
2016-07-26 16:59:10 -0400

    train_model now correctly parses OMEtiff input

--- tools/cellorganizer/train_model.sh
+++ tools/cellorganizer/train_model.sh
@@ -4,27 +4,30 @@ export PATH=$PATH:$(dirname $0)
 
 WORKING_DIRECTORY=`pwd`
 DATASET=$1
-MASKSET=$2
-TRAINFLAG=$3
-MODELNAME=$4
-MODELID=$5
-DOWNSAMPLE=$6
-NUCLEUSTYPE=$7
-NUCLEUSNAME=$8
-NUCLEUSCLASS=$9
-NUCLEUSID=$10
-CELLTYPE=$11
-CELLNAME=$12
-CELLCLASS=$13
-CELLID=$14
-PROTEINTYPE=$15
-PROTEINNAME=$16
-PROTEINCLASS=$17
-PROTEINID=$18
-PROTEINCYTONUCLEARFLAG=$19
-DOCUMENTATION=$20
-VERBOSE=$21
-DEBUG=$22
+DNACHANNEL=$2
+CELLCHANNEL=$3
+PROTEINCHANNEL=$4
+MASKSET=$5
+TRAINFLAG=$6
+MODELNAME=$7
+MODELID=$8
+DOWNSAMPLE=$9
+NUCLEUSTYPE=$10
+NUCLEUSNAME=$11
+NUCLEUSCLASS=$12
+NUCLEUSID=$13
+CELLTYPE=$14
+CELLNAME=$15
+CELLCLASS=$16
+CELLID=$17
+PROTEINTYPE=$18
+PROTEINNAME=$19
+PROTEINCLASS=$20
+PROTEINID=$21
+PROTEINCYTONUCLEARFLAG=$22
+DOCUMENTATION=$23
+VERBOSE=$24
+DEBUG=$25
 
 echo "
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -91,23 +94,29 @@ options.debug = $DEBUG;
 
 masks = $MASKSET
 
-%%% parese image data
+%%% pares image data
 f1 = fopen(dataset);
 imagepaths = textscan(f1,'%s\n');
 imagepaths = imagepaths{1};
 fclose(f1);
 
 %%% get dna imagepaths
-dnainds = ~cellfun(@isempty,regexp(imagepaths,'ch0'));
-dnapaths = imagepaths(dnainds);
+dna_channel_numbers = OME_getChannelNumbers(imagepaths, $DNACHANNEL)
+dna_paths = [imagepaths,dna_channel_numbers];
+%dnainds = ~cellfun(@isempty,regexp(imagepaths,'ch0'));
+%dnapaths = imagepaths(dnainds);
 
 %%% get cell imagepaths
-cellinds = ~cellfun(@isempty,regexp(imagepaths,'ch1'));
-cellpaths = imagepaths(cellinds);
+cell_channel_numbers = OME_getChannelNumbers(imagepaths, $CELLCHANNEL)
+cell_paths = [imagepaths,cell_channel_numbers];
+%cellinds = ~cellfun(@isempty,regexp(imagepaths,'ch1'));
+%cellpaths = imagepaths(cellinds);
 
 %%% get prot image paths
-protinds = cellinds+dnainds<1;
-protpaths = imagepaths(protinds);
+protein_channel_numbers = OME_getChannelNumbers(imagepaths, $PROTEINCHANNEL)
+dna_paths = [imagepaths,protein_channel_numbers];
+%protinds = cellinds+dnainds<1;
+%protpaths = imagepaths(protinds);
 
 %%% get cell mask paths
 try
@@ -120,26 +129,36 @@ catch
     disp('no mask images found')
 end
 
-reader = bfGetReader(imagepaths{1});
+%%% get image resolution and dimensionality
+options.model.resolution = OME_getResolution(imagepaths{1});
+
+reader = bfGetReader(image_path);
 omeMeta = reader.getMetadataStore();
-
-xrange = omeMeta.getPixelsSizeX(0).getValue();
-xpixelsize = omeMeta.getPixelsPhysicalSizeX(0).value();
-
-yrange = omeMeta.getPixelsSizeY(0).getValue();
-ypixelsize = omeMeta.getPixelsPhysicalSizeY(0).value();
-
-try
-    zrange = omeMeta.getPixelsSizeZ(0).getValue();
-    zpixelsize = omeMeta.getPixelsPhysicalSizeZ(0).value();
+if omeMeta.getPixelsSizeZ(0).getValue()>2
     dimensionality = '3D';
-    options.model.resolution = [xrange/xpixelsize yrange/ypixelsize zrange/zpixelsize];
-    options.model.original_resolution = [xrange/xpixelsize yrange/ypixelsize zrange/zpixelsize];
-catch
+else
     dimensionality = '2D';
-    options.model.resolution = [xrange/xpixelsize yrange/ypixelsize];
-    options.model.original_resolution = [xrange/xpixelsize yrange/ypixelsize];
-end
+
+%reader = bfGetReader(imagepaths{1});
+%omeMeta = reader.getMetadataStore();
+
+%xrange = omeMeta.getPixelsSizeX(0).getValue();
+%xpixelsize = omeMeta.getPixelsPhysicalSizeX(0).value();
+
+%yrange = omeMeta.getPixelsSizeY(0).getValue();
+%ypixelsize = omeMeta.getPixelsPhysicalSizeY(0).value();
+
+%try
+%    zrange = omeMeta.getPixelsSizeZ(0).getValue();
+%    zpixelsize = omeMeta.getPixelsPhysicalSizeZ(0).value();
+%    dimensionality = '3D';
+%    options.model.resolution = [xrange/xpixelsize yrange/ypixelsize zrange/zpixelsize];
+%    options.model.original_resolution = [xrange/xpixelsize yrange/ypixelsize zrange/zpixelsize];
+%catch
+%    dimensionality = '2D';
+%    options.model.resolution = [xrange/xpixelsize yrange/ypixelsize];
+%    options.model.original_resolution = [xrange/xpixelsize yrange/ypixelsize];
+%end
 
 answer = img2slml(dimensionality, dnapath, cellpath, proteinpath, options);
 
