f8aaaec58c140baf6214dd3bc0809ca0dd0624ab
Tim Majarian <tmajarian@cmu.edu>
2016-08-02 14:56:58 -0400

    created model_training test script (currently not working)

---
+++ model_training_test.sh
@@ -0,0 +1,36 @@
+dataset=/Users/timmajarian/Documents/Murphylab/projects/galaxy_chapter/images/omero_imgs.txt
+dnachannel=1
+cellchannel=2
+proteinchannel=3
+masks=""
+trainflag="all"
+modelname="hela_test"
+modelid="1"
+downsample=5
+nucleustype="diffeomorphic"
+nucleusclass="framework"
+nucleusid="1"
+celltype="diffeomorphic"
+cellname="test_hela_cell"
+cellclass="framework"
+cellid="1"
+proteintype="gmm"
+proteinname="mito"
+proteinclass="vesicle"
+proteinid="1"
+proteincytonuclearflag="cyto"
+documentation="testing"
+verbose="true"
+debug="false"
+
+cd tools/cellorganizer
+rm -fv ./cellorganizer
+bash ./train_model.sh $dataset $dnachannel $cellchannel $proteinchannel $masks $trainflag $modelname $modelid $downsample $nucleustype $nucleusname $nucleusclass $nucleusid $celltype $cellname $cellclass $cellid $proteintype $proteinname $proteinclass $proteinid $proteincytonuclearflag $documentation $verbose $debug
+
+FILE1=output1.mat
+if [ -f $FILE1 ]; then
+	echo "File exists"
+else
+	echo "Output file $FILE1 does not exist."
+	exit -1
+fi
