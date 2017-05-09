cd321c852afdc467487d272ced1b3735de68993b
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2017-05-08 22:36:35 -0400

    Updated script.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 cp galaxy.ini galaxy/config
 cp tool_conf.xml galaxy/config
 cp datatypes_conf.xml galaxy/config
-cp requirements.txt galaxy/requirements.txt
+#cp requirements.txt galaxy/requirements.txt
 rsync -ruv datatypes/ galaxy/lib/galaxy/datatypes/
 rsync -ruv tools/ galaxy/tools/
 rsync -ruv tool-data/ galaxy/tool-data # for future usage
