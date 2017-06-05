0bd0680a6b1931e9745c75f9f8bc1af5a1398840
xlu2 <xlu2@andrew.cmu.edu>
2017-06-05 16:33:12 -0400

    add

--- tools/cellorganizer/ometiff_validation.sh
+++ tools/cellorganizer/ometiff_validation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
 if ! $bftools/xmlvalid $1 | grep -q "No validation errors found."; then
-    error="Errors found in the ometiff file !"
-    echo $error
+    exit -1
+else
+	cp $1 pwd
+	exit 0
 fi
