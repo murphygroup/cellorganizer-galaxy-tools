2bf068f3d2a8f23495943ff81b6794d023d83fb8
xlu2 <xlu2@andrew.cmu.edu>
2017-06-05 18:03:34 -0400

    add

--- tools/cellorganizer/ometiff_validation.sh
+++ tools/cellorganizer/ometiff_validation.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-if ! $bftools/xmlvalid $1 | grep -q "No validation errors found."; then
+
+ln -s $1 ./valid_image.ome.tif
+ls -lt
+xmlvalid valid_image.ome.tif
+
+if ! xmlvalid valid_image.ome.tif | grep -q "No validation errors found."; then
+	rm -f valid_image.ome.tif
+	ls -lt
     exit -1
 else
-	cp $1 pwd
 	exit 0
 fi
