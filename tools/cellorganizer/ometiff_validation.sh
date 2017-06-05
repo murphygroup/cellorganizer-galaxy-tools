84412746122057e872630bed3289d07cc8271d23
xlu2 <xlu2@andrew.cmu.edu>
2017-06-05 18:06:17 -0400

    add

--- tools/cellorganizer/ometiff_validation.sh
+++ tools/cellorganizer/ometiff_validation.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 ln -s $1 ./valid_image.ome.tif
-ls -lt
+
 xmlvalid valid_image.ome.tif
 
 if ! xmlvalid valid_image.ome.tif | grep -q "No validation errors found."; then
 	rm -f valid_image.ome.tif
-	ls -lt
     exit -1
 else
 	exit 0
