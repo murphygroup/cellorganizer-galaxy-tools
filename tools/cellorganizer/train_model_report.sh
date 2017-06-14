68ff97ef715ae48dcf599d214096f35a75c64faa
xlu2 <xlu2@andrew.cmu.edu>
2017-06-14 17:20:02 -0400

    add

--- tools/cellorganizer/train_model_report.sh
+++ tools/cellorganizer/train_model_report.sh
@@ -1,15 +1,15 @@
 zip -r param.zip param
 
-# file log.txt
+file diary.txt
 file model.mat
 file param.zip
 # file temp.zip
 
 echo "# Demo1D01 " > report.md
-echo "# Logs " >> report.md
-# echo " " >> report.md
-# cat log.txt >> report.md
-# echo " " >> report.md
+echo "# diarys " >> report.md
+echo " " >> report.md
+cat diary.txt >> report.md
+echo " " >> report.md
 echo "![Model Download](./model.mat)" >> report.md
 echo " " >> report.md
 echo "![Parameterization Download](./param.zip)" >> report.md
@@ -20,7 +20,7 @@ echo "Making temporary folder "$1
 if [ ! -d $1 ]; then
         mkdir $1
 fi
-# mv log.txt $1
+mv diary.txt $1
 mv model.mat $1
 mv param.zip $1
 # mv temp.zip $1
