6972a60925d1a458acbe47dad64b9cc020a6ab4a
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2017-07-11 17:23:07 -0400

    Updated model2info.

--- tools/cellorganizer/model2info.sh
+++ tools/cellorganizer/model2info.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-    
+
+echo "Generating Markdown file"    
 echo "# model2info tool output" > report.md
 
 if [ -f diary.txt ]; then
@@ -7,7 +8,6 @@ if [ -f diary.txt ]; then
 	cat diary.txt >> report.md
 	echo "\`\`\`" >> report.md
 fi
-
 
 echo "Making temporary folder "$1
 if [ ! -d $1 ]; then
@@ -18,4 +18,5 @@ if [ -f diary.txt ]; then
     cp -v diary.txt $1
 fi
 
+echo "Parsing Markdown file into HTML file"
 pandoc report.md -o report.html
