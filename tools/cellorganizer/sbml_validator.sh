d057129402079da78fbbe3dc6999b7be41fa10f1
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2017-07-11 15:09:35 -0400

    Updated script to save output in TXT file.

--- tools/cellorganizer/sbml_validator.sh
+++ tools/cellorganizer/sbml_validator.sh
@@ -3,4 +3,5 @@ dataset=$1
 echo ln -s $dataset ./file.xml
 ln -s $dataset ./file.xml
 zip -v file.zip file.xml
-curl --silent -F file=@file.zip -F output=xml -F offcheck=u,r -F apikey='d40d197d-dee1-4275-baf8-44d6016915e2' http://sbml.org/validator/ 2>&1
+curl --silent -F file=@file.zip -F output=xml -F offcheck=u,r -F apikey='d40d197d-dee1-4275-baf8-44d6016915e2' http://sbml.org/validator/ 2>&1 >> output.txt
+cat output.txt
