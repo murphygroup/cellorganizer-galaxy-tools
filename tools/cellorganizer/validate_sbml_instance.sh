0f29732689fc1b1ec6a237882ebec59e2710e6c2
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2017-07-17 12:02:54 -0400

    Updated tool.

--- tools/cellorganizer/validate_sbml_instance.sh
+++ tools/cellorganizer/validate_sbml_instance.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
 dataset=$1
+echo "Linking dataset to local workspace"
 echo ln -s $dataset ./file.xml
 ln -s $dataset ./file.xml
+
+echo "Compressing SBML instance"
 zip -v file.zip file.xml
+
+echo "Validating instance"
 curl --silent -F file=@file.zip -F output=xml -F offcheck=u,r -F apikey='d40d197d-dee1-4275-baf8-44d6016915e2' http://sbml.org/validator/ 2>&1 >> output.txt
+
 cat output.txt
