543e63a43a36d118fdabe586f2b0e327fabf517f
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2017-07-27 16:33:20 -0400

    Cleaned up tools.

--- tools/cellorganizer/sample_from_database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PATH=$PATH:$(dirname $0)
-
-WORKING_DIRECTORY=`pwd`
-
-OMERO_ID=`python -c "from __future__ import print_function; import random; print(random.choice(range(51, 251) + range(304, 425) + range(451, 532)), end='')"`
-
-wget --quiet -O output.tif -o /dev/null -nc "http://omero.compbio.cs.cmu.edu:8080/webgateway/render_ome_tiff/i/"$OMERO_ID"/"
