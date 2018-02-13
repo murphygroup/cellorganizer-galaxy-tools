584bacc958a95318b4c9abb6c69448696939c127
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2018-02-13 17:36:21 -0500

    Updated download script

---
+++ tools/cellorganizer/download_models_from_repository.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dataset=$1
+
+echo "Downloading "$dataset
+echo wget --quiet -O model.mat -nc http://www.cellorganizer.org/model_repository/$dataset
+
+find . -type f -empty -exec rm -fv {} \;
+
+if [ ! f model.mat ]; then
+		echo "File does not exist"
+		exit -1
+fi
