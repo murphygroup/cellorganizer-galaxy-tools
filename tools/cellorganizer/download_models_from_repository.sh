b8f0f2dfb943b927fc578b2f0f380830a10fbac3
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2018-02-14 15:22:14 -0500

    Updated links and fixed issue.

--- tools/cellorganizer/download_models_from_repository.sh
+++ tools/cellorganizer/download_models_from_repository.sh
@@ -3,11 +3,13 @@
 dataset=$1
 
 echo "Downloading "$dataset
+echo wget --quiet -O model.mat -nc http://www.cellorganizer.org/model_repository/$dataset
 wget --quiet -O model.mat -nc http://www.cellorganizer.org/model_repository/$dataset
 
 find . -type f -empty -exec rm -fv {} \;
 
+find . -type f
+
 if [ ! -f model.mat ]; then
 		echo "File does not exist"
-		exit -1
 fi
