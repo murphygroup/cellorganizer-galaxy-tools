59f2217c5a53e6fb916a4ea9e597a24d5de11664
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2018-02-19 16:35:05 -0500

    Fixed eternal bug! We can now import Matlab files from generic UI

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ cp galaxy.ini galaxy/config
 cp tool_conf.xml galaxy/config
 cp datatypes_conf.xml galaxy/config
 #cp requirements.txt galaxy/requirements.txt
-rsync -ruv datatypes/ galaxy/lib/galaxy/datatypes/
+cp binary.py galaxy/lib/galaxy/datatypes/
+cp registry.py galaxy/lib/galaxy/datatypes/
 rsync -ruv tools/ galaxy/tools/
 rsync -ruv tool-data/ galaxy/tool-data # for future usage
 cp welcome.html galaxy/static/
