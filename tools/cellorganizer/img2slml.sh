e35ecfc216839c8d114e9ba4574d513fe313c12a
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2018-03-08 16:34:32 -0500

    Updated training tool

--- tools/cellorganizer/img2slml.sh
+++ tools/cellorganizer/img2slml.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-SCRIPT=$1
-cat $SCRIPT | matlab -nodesktop -nosplash
-rm -fv $SCRIPT
+FILE=`cat script.m`
+echo $FILE | matlab -nodesktop -nosplash
+
+rm -fv script.m
+
+if [ ! -d $1 ]; then
+	mkdir $1
+fi
+
+if [ -f model.mat ]; then
+	cp -v model.mat $1/
+fi
