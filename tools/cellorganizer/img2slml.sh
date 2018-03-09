31155eb59ce9ac521df21e49bf227acd235845d9
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2018-03-09 05:33:51 -0500

    Updated tools

--- tools/cellorganizer/img2slml.sh
+++ tools/cellorganizer/img2slml.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+bash ./check_if_files_exist_on_disk_and_link_them.sh "$1"
+
 cat script.m | matlab -nodesktop -nosplash
 
-if [ ! -d $1 ]; then
-	mkdir $1
+if [ ! -d $2 ]; then
+	mkdir $2
 fi
 
 if [ -f model.mat ]; then
 	cp -v model.mat $1/
 fi
-
-rm -fv ./image*.ome.tif
