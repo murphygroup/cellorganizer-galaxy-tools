3aa24f6625097b0d7ec2aaedb55e92b011e1b718
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2018-03-10 00:29:28 -0500

    Fixed blocked bug

--- tools/cellorganizer/img2slml.sh
+++ tools/cellorganizer/img2slml.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-bash ./check_if_files_exist_on_disk_and_link_them.sh "$1"
+bash ./check_if_files_exist_on_disk_and_link_them.sh "$1" 2>/dev/null 
 
-cat script.m | matlab -nodesktop -nosplash
+#cat script.m | matlab -nodesktop -nosplash
+matlab -nodesktop -nosplash -r "script;" 2>/dev/null 
 
 if [ ! -d $2 ]; then
 	mkdir $2
 fi
 
 if [ -f model.mat ]; then
-	cp -v model.mat $1/
+	cp -v model.mat $2/
 fi
