6b590a308b21f46ccf51d451edb219d722df7669
bteo <bteo@andrew.cmu.edu>
2018-03-16 17:23:53 -0400

    Updated conditional within import_and_validate_ometiff.sh

--- tools/cellorganizer/import_and_validate_ometiff.sh
+++ tools/cellorganizer/import_and_validate_ometiff.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-wget --quiet -O image.ome.tif -o /dev/null -nc $1
+echo "Downloading "$1
+echo wget --quiet -O image.ome.tif -nc $1
+wget --quiet -O image.ome.tif -nc $1 2>/dev/null
 
 echo "Checking file"
-file ./image.ome.tif
+find . -type f -empty -exec rm -fv {} \;
+ls
+if [ ! -f image.ome.tif ]; then
+    echo "File does not exist"
+    exit 1
+fi
 
 echo "Validating file"
-xmlvalid ./image.ome.tif
 
-if ! xmlvalid ./image.ome.tif | grep -q "No validation errors found."; then
-	echo "File is not valid"
-	rm -f ./image.ome.tif
-	exit -1
+xmlvalid ./image.ome.tif | grep -q "No validation errors found."
+if (($? == 0)); then
+    echo "File is valid"
+    exit 0
 else
-	echo "File is valid"
-	exit 0
+    echo "File is not valid"
+    rm -f ./image.ome.tif
+    exit -1
 fi
