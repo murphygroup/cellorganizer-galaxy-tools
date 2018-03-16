89ba7f88c68ce1c248388417db71b08ff7784e83
Ulani Qi <uhq@andrew.cmu.edu>
2018-03-16 16:34:27 -0400

    added tours for get data, changed run.sh to copy yaml files to tours folder, commented out file validation for import_and_validate_ometiff.sh

--- tools/cellorganizer/import_and_validate_ometiff.sh
+++ tools/cellorganizer/import_and_validate_ometiff.sh
@@ -5,14 +5,14 @@ wget --quiet -O image.ome.tif -o /dev/null -nc $1
 echo "Checking file"
 file ./image.ome.tif
 
-echo "Validating file"
-xmlvalid ./image.ome.tif
+#echo "Validating file"
+#xmlvalid ./image.ome.tif
 
-if ! xmlvalid ./image.ome.tif | grep -q "No validation errors found."; then
-	echo "File is not valid"
-	rm -f ./image.ome.tif
-	exit -1
-else
-	echo "File is valid"
-	exit 0
-fi
+#if ! xmlvalid ./image.ome.tif | grep -q "No validation errors found."; then
+#	echo "File is not valid"
+#	rm -f ./image.ome.tif
+#	exit -1
+#else
+#	echo "File is valid"
+#	exit 0
+#fi
